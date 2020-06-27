28913443b0f4bd0a3380beb93b53d0b21646724e
DESKTOP-STRK401\hagirainbow <h8a9g11i@gmail.com>
2020-06-27 22:00:51 +0900

    ファイル名選択まで実装

---
+++ BinalyTest/FileIO.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BinalyTest
+{
+
+    class FileIO
+    {
+        /// <summary>
+        /// 開くファイル名の選択
+        /// </summary>
+        /// <param name="nowText">現在の選択中ファイル名</param>
+        /// <returns>選択変更後の選択中ファイル名</returns>
+        internal static string openFileSelect(string nowText)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            if (dialog.ShowDialog() == DialogResult.OK) return dialog.FileName;
+            // キャンセル時・例外時などは、今のテキストをそのまま返す
+            return nowText;
+        }
+
+        /// <summary>
+        /// 保存するファイル名の選択
+        /// </summary>
+        /// <param name="nowText">現在の選択中ファイル名</param>
+        /// <returns>選択変更後の選択中ファイル名</returns>
+        internal static string saveFileSelect(string nowText)
+        {
+            SaveFileDialog dialog = new SaveFileDialog();
+            if (dialog.ShowDialog() == DialogResult.OK) return dialog.FileName;
+            // キャンセル時・例外時などは、今のテキストをそのまま返す
+            return nowText;
+        }
+    }
+}
--- BinalyTest/Form1.cs
+++ BinalyTest/Form1.cs
@@ -16,5 +16,40 @@ namespace BinalyTest
         {
             InitializeComponent();
         }
+
+        /// <summary>
+        /// 読み込みファイル名選択ボタン押下時処理
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            textBox1.Text = FileIO.openFileSelect(textBox1.Text);
+            button1.Enabled = true;
+        }
+
+        /// <summary>
+        /// 書き込みファイル名選択ボタン押下時処理
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button2.Enabled = false;
+            textBox2.Text = FileIO.saveFileSelect(textBox1.Text);
+            button2.Enabled = true;
+        }
+
+        /// <summary>
+        /// 変換ボタン押下時処理
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button3_Click(object sender, EventArgs e)
+        {
+            button3.Enabled = false;
+            button3.Enabled = true;
+        }
     }
 }
